64a4c149a5b5815515f5ece81d3028de3834442a
Ernesto Afane <ernesto@97thfloor.com>
2026-06-04 18:59:16 -0600

    ecosystem multiple files

---
+++ ecosystem/ecosystem.testing.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "base-portal",
+      script: "node_modules/.bin/next",
+      args: "start",
+      cwd: "/var/www/html/base-portal.97dev.com",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3777,
+      },
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "./logs/error.log",
+      out_file: "./logs/output.log",
+      merge_logs: true,
+      max_restarts: 10,
+      min_uptime: "10s",
+      max_memory_restart: "512M",
+      watch: false,
+      ignore_watch: ["node_modules", ".next", "logs"],
+    },
+  ],
+};
