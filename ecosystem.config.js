ad28dad45fa44031e9b5d113b33e8cb7c23c3488
Ernesto Afane <ernesto@97thfloor.com>
2026-06-04 19:08:23 -0600

    ecosystem config updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,6 @@
 // Default config — points to testing environment.
 // For other environments use:
+//   pm2 start ecosystem/ecosystem.testing.config.js
 //   pm2 start ecosystem/ecosystem.staging.config.js
 //   pm2 start ecosystem/ecosystem.production.config.js
-module.exports = require("./ecosystem/ecosystem.testing.config.js");
+module.exports = require("./ecosystem/ecosystem.production.config.js");
